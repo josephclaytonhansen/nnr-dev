3cedb9bcdd6fb490cb76eec18e76c8d9113d453b
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-06-19 13:08:34 -0500

    update constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://4035c41779580f.lhr.life'
+export const BASE_URL = 'https://8983d0a2d779bd.lhr.life'
 export const RECIPES_URL = `/api/recipes`
 export const USERS_URL = `/api/users`
 export const COMMENTS_URL = `/api/comments`
