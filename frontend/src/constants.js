8e0d5314c0fa9972d5301ae8ce9a0f04bb1b4dbd
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-06-26 14:59:43 -0500

    starting admin middleware

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://7dc47baeb770e5.lhr.life'
+export const BASE_URL = 'https://da5d969fd19313.lhr.life'
 export const RECIPES_URL = `/api/recipes`
 export const USERS_URL = `/api/users`
 export const COMMENTS_URL = `/api/comments`
