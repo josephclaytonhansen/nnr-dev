544a942ee238fcbfbdd4edc84cacaaf40cebfcb1
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-07-18 14:37:25 -0500

    separate front-end repo

--- frontend/src/components/Message.jsx
+++
@@ -1,8 +0,0 @@
-import { Alert } from 'react-bootstrap'
-export default function Message({ variant='info', style ={transition:'all 0s'}, children }) {
-    return (
-        <>
-            <Alert className='' style = {style} variant={variant}>{children}</Alert>
-        </>
-    )
-}
